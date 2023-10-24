98bfc0d2d3cd532a2efdb80ed820bc4152c22005
Kkonk <konk99@o2.pl>
2023-10-24 16:18:29 +0200

    Testing

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Route, Routes} from 'react-router-dom';
+import Contact from "../Contact/Contact";
+import HUD from "../HUD/HUD";
+import Project from "../Project/Project";
+import About from "../About/About";
+import Main from "../Main/Main";
+
+const App = () => (
+
+        <HUD>
+            <Routes>
+                <Route exact="true" path = '/' component = {Main} />
+                <Route exact="true" path = '/contact' component = {Contact} />
+                <Route exact="true" path = '/project' component = {Project} />
+                <Route exact="true" path = '/about' component = {About} />
+            </Routes>
+        </HUD>
+
+);
+
+export default App;
