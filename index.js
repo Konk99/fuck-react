44428afae9cc108cff1f2c24255b51d9b7352b85
Kkonk <konk99@o2.pl>
2023-10-24 18:49:05 +0200

    Updates

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/fuck-react/*' element={<App />}>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
+
